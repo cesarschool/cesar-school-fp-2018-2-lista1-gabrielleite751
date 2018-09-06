176b3c25057262373ee72295d2cfb9e28fd87073
gabrielleite751 <42241006+gabrielleite751@users.noreply.github.com>
2018-09-05 22:40:55 -0300

    Update questao_3.py

--- questoes/questao_3.py
+++ questoes/questao_3.py
@@ -10,10 +10,20 @@
 # Para a correta execução do programa, a estrutura atual deve ser mantida,
 # substituindo apenas o comando print(questão...) existente.
 ##
+from math import *
+
 def main():
     print("questao 3")
+r = float(input("Qual o raio do Círculo? "))
 
+diam = float(2*r)
+area = float(pi * (r**2))
+circ = float(2*pi*r)
 
-    
+print("Com um raido de " + str(r) + " Temos:")
+print("Diâmetro " + str(diam))
+print("Área " + str(area))
+print("Comprimento: " + str(circ))
+
 if __name__ == '__main__':
-    main()
+ main()
